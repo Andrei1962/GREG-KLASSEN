a67969ba0113d51675a2faca5c36d0100b33fc39
Andrei Stolarov <andrey.stolyarov.1962@mail.ru>
2025-02-02 02:02:40 +0300

    слайдер

---
+++ Desktop/FITNESS-CLUB GRID/script.js
@@ -0,0 +1,23 @@
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    },
+  // навигация
+  navigation: {
+  nextEl: '.swiper-button-next',
+  prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+      640: {
+      slidesPerView: 2,
+      },
+      768: {
+      slidesPerView: 4,
+      },
+      1024: {
+      slidesPerView: 5,
+      },
+  },
+});
